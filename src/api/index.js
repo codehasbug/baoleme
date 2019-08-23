403dcf70f1727d95f20a21c519b94fd23465f1f2
JasonYang <1960228744@qq.com>
2019-08-23 10:06:38 +0800

    commit

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+const origin = "http://ustbhuangyi.com/sell/";
+
+const getSeller = origin + "api/seller";
+const getGoods = origin + "api/goods";
+const getRatings = origin + "api/ratings";
+
+export { getSeller, getGoods, getRatings };
