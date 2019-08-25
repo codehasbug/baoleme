4c163ec4c681bee70322386948068d5faa39064f
fu <1804528105@qq.com>
2019-08-25 15:46:42 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <seller></seller>
+  </div>
 </template>
 
-<style></style>
+<script>
+  import seller from "./components/seller/seller";
+  export default {
+    name: 'app',
+    data(){
+
+    },
+    components: {
+      seller
+    },
+
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
