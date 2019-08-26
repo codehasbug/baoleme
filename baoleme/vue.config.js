f3bb65eab5f1aac7a2ee23e3286cb23c56478a57
chenglaiyong <37152804+chenglaiyong@users.noreply.github.com>
2019-08-26 09:12:48 +0800

    Add files via upload

---
+++ baoleme/vue.config.js
@@ -0,0 +1,46 @@
+const MoocData = require("./data.json");
+const seller = MoocData.seller;
+const goods = MoocData.goods;
+const ratings = MoocData.ratings;
+
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map"
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        "resolve url": true,
+        import: ["./src/theme"]
+      }
+    }
+  },
+  pluginOptions: {
+    "cube-ui": {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get("/api/seller", (req, res) => {
+        res.json({
+          errno: 0,
+          data: seller
+        });
+      });
+      app.get("/api/goods", (req, res) => {
+        res.json({
+          errno: 0,
+          data: goods
+        });
+      });
+      app.get("/api/ratings", (req, res) => {
+        res.json({
+          errno: 0,
+          data: ratings
+        });
+      });
+    }
+  }
+};
